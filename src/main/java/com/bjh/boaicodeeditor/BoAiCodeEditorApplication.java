730ab9fb7fc9671386639a230f655b9607d15d7d
薄钧涵 <junhanbo87@gmail.com>
2025-09-08 22:19:03 +0800

    初始化项目

---
+++ src/main/java/com/bjh/boaicodeeditor/BoAiCodeEditorApplication.java
@@ -0,0 +1,13 @@
+package com.bjh.boaicodeeditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoAiCodeEditorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoAiCodeEditorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bjh/boaicodeeditor/BoAiCodeEditorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjh.boaicodeeditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoAiCodeEditorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
