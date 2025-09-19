0341d5b70ba306291f36733f6a00f45092c1fab6
薄钧涵 <junhanbo87@gmail.com>
2025-09-19 11:52:56 +0800

    完成基本功能 应用模块基本功能

---
+++ src/main/java/com/bjh/boaicodeeditor/model/dto/app/AppAddRequest.java
@@ -0,0 +1,19 @@
+package com.bjh.boaicodeeditor.model.dto.app;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * app 创建请求
+ */
+@Data
+public class AppAddRequest implements Serializable {
+
+    /**
+     * 应用初始化的 prompt
+     */
+    private String initPrompt;
+
+    private static final long serialVersionUID = 1L;
+}
--- src/main/java/com/bjh/boaicodeeditor/service/AppService.java
+++ src/main/java/com/bjh/boaicodeeditor/service/AppService.java
@@ -1,13 +1,36 @@
 package com.bjh.boaicodeeditor.service;
 
+import com.bjh.boaicodeeditor.model.dto.app.AppQueryRequest;
+import com.bjh.boaicodeeditor.model.vo.AppVO;
+import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.core.service.IService;
 import com.bjh.boaicodeeditor.model.entity.App;
+
+import java.util.List;
 
 /**
  * 应用 服务层。
  *
- * @author Defector
  */
 public interface AppService extends IService<App> {
+
+    /**
+     * 获取应用封装类
+     */
+
+    AppVO getAppVO(App app);
+
+    /**
+     * 获取应用封装类列表
+     */
+    List<AppVO> getAppVOList(List<App> appList);
+
+    /**
+     * 构造应用查询条件
+     * @param appQueryRequest
+     * @return
+     */
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
+
 
 }
