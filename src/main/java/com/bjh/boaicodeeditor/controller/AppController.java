fe3678312ea4aa2946903376bf810756129bb856
薄钧涵 <junhanbo87@gmail.com>
2025-09-25 22:21:24 +0800

    系统优化 redis缓存优化 旁路缓存，配置redis缓存管理器，使用Spring Data Redis 组件提供的缓存注解

--- src/main/java/com/bjh/boaicodeeditor/controller/AppController.java
+++ src/main/java/com/bjh/boaicodeeditor/controller/AppController.java
@@ -24,6 +24,7 @@ import com.mybatisflex.core.query.QueryWrapper;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.*;
@@ -237,6 +238,11 @@ public class AppController {
      * @return 应用列表
      */
     @PostMapping("/my/list/page/vo")
+    @Cacheable(
+            value = "good_app_page",
+            key = "T(com.bjh.boaicodeeditor.utils.CacheKeyUtils).generateKey(#appQueryRequest)",
+            condition = "#appQueryRequest.pageNum <= 10"
+    )
     public BaseResponse<Page<AppVO>> listMyAppVOByPage(@RequestBody AppQueryRequest appQueryRequest, HttpServletRequest request) {
         ThrowUtils.throwIf(appQueryRequest == null, ErrorCode.PARAMS_ERROR);
         User loginUser = userService.getLoginUser(request);
